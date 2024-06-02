e24c98731eb97a4d601916b749f639e206ba43f5
Aman Hossain <amanhossainmail@gmail.com>
2024-06-02 17:40:38 +0530

    Work Added

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,5 @@
-greetings = 'Hello World!'
-print(greetings)
+num1 = int(input("Please enter the first number: ")) 
+num2 = int(input("Please enter the second number: ")) 
+result = num1 + num2 
+print (result)
+print (num1, type(num1), num2, type(num2)) 
